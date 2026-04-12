dc32136e212fd9798b327b824908b57cc0126c71
Jack Myrick <contact@jackmyrick.design>
2026-04-12 17:50:06 +0100

    smoothing the stones now doesn't pause them mid air

--- Assets/BugBox/Testing/Terrain/Scripts/DrainageStone.cs
+++ Assets/BugBox/Testing/Terrain/Scripts/DrainageStone.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class DrainageStone : MonoBehaviour
 {
+    [SerializeField] float m_SettleTime = 1f;  // seconds to wait after spawning before allowing kinematic
+
     Rigidbody m_Rigidbody;
+    bool m_SmoothingPending;
+    float m_TimeAlive;
 
     void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
+    void Update()
+    {
+        m_TimeAlive += Time.deltaTime;
+
+        if (m_SmoothingPending && m_TimeAlive >= m_SettleTime)
+        {
+            m_Rigidbody.isKinematic = true;
+            m_SmoothingPending = false;
+        }
+    }
+
     public void SetSmoothing(bool smoothing)
     {
-        m_Rigidbody.isKinematic = smoothing;
+        if (smoothing)
+        {
+            if (m_TimeAlive >= m_SettleTime)
+                m_Rigidbody.isKinematic = true;
+            else
+                m_SmoothingPending = true;
+        }
+        else
+        {
+            m_SmoothingPending = false;
+            m_Rigidbody.isKinematic = false;
+        }
     }
 }

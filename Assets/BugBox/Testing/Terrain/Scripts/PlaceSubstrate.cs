c4422cb78e2da16df3d428d0ce6680ce5a73127f
Jack Myrick <contact@jackmyrick.design>
2026-04-12 23:08:20 +0100

    created place substrate script

---
+++ Assets/BugBox/Testing/Terrain/Scripts/PlaceSubstrate.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using CustomAttributes;
+
+public class PlaceSubstrate : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private JarManager Manager;
+    [SerializeField] private GameObject SubstratePrefab;
+    [SerializeField] private GameObject SubstrateParent;
+
+    [Header("Spawning")]
+    [SerializeField] private Vector3 SpawnOffset;
+    [ReadOnly] public Transform BarrierTransform;
+
+    public void CreateSubstrate()
+        {
+            if (BarrierTransform != null)
+            {
+                GameObject substrate = Instantiate(SubstratePrefab, (BarrierTransform.position + SpawnOffset), Quaternion.identity, SubstrateParent.transform);
+                //Here I can add code to disable components on the Drainage stones
+            }
+        else { Debug.LogError("BarrierTransform is not set. Please place the substrate barrier first."); }
+    }
+}
